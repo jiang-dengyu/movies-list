09e8cd42c45fc94f728adb46f72228057e7d9672
jiang-dengyu <kennykenny19970314@gmail.com>
2024-01-11 13:08:25 +0800

    feat: add listing page

--- app.js
+++ app.js
@@ -2,6 +2,8 @@ const express  = require('express')
 const { engine } = require('express-handlebars')
 const app = express()
 const port = 3000
+const movies = require('./public/jsons/movies.json').results
+const BASE_IMG_URL = 'https://movie-list.alphacamp.io/posters/'
 
 app.engine('.hbs', engine({extname: '.hbs'}));
 app.set('view engine', '.hbs');
@@ -12,9 +14,8 @@ app.use(express.static('public'))
 app.get ('/',(req,res)=>{
   res.redirect('/movies')
 })
-
 app.get('/movies',(req,res)=>{
-  res.render('index')
+  res.render('index',{movies, BASE_IMG_URL})
 })
 
 app.get('/movie/:id',(req,res)=>{
@@ -25,3 +26,4 @@ app.get('/movie/:id',(req,res)=>{
  app.listen(port,()=>{
   console.log(`express server is running on http://localhost:${port}`)
  })
+ 
